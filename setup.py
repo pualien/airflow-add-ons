d57cb99064599820cdd173c271429c7a43b85b03
msenardi <pulien@gmail.com>
2020-11-25 03:58:01 +0100

    testing GmailImapEmailSensor

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.1.16',
+      version='0.1.17',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
