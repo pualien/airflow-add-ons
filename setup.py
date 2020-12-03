b54afda98ac6612688fc4f2ce3c4102dbc62c39b
msenardi <pulien@gmail.com>
2020-12-03 15:40:37 +0100

    cleaned requirements

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.1.19',
+      version='0.1.20',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
