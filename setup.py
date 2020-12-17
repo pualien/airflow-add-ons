073c1e59c03485a7c954cdef4a43faa5fbae06bb
msenardi <pulien@gmail.com>
2020-12-17 16:16:24 +0100

    fix version boto3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.1.22',
+      version='0.1.23',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
