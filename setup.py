85b3d3c41c3ad2992c6c5a8f4045ecab97b687da
msenardi <pulien@gmail.com>
2021-02-12 18:50:25 +0100

    testing new providers

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.2.0',
+      version='0.2.0b',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
