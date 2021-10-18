ed4f1478e5c86e5a860db2cd3e18da8c59cf978b
Matteo Senardi <pualien@gmail.com>
2021-10-18 15:10:57 +0200

    upgraded provider packages

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.2.3',
+      version='0.2.4',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
