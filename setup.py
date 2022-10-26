32e1daa240aa2a8eba7cbcb5a1349cd84e124b2c
Matteo Senardi <pualien@gmail.com>
2022-10-26 14:34:21 +0200

    restored previous version with updated packages

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def reqs(*f):
 
 
 setup(name='airflow-add-ons',
-      version='0.2.9b2',
+      version='0.2.10',
       url='https://github.com/pualien/airflow-add-ons',
       license='MIT',
       author='Matteo Senardi',
